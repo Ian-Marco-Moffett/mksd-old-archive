3eabaa460f1e657daa1356fc42b07e98425cc7c1
Ian Moffett <spurious.vector@gmail.com>
2022-12-14 03:41:06 -0500

    Feat: Use preprocessor for program flexibility.

--- sys/src/init.c
+++ sys/src/init.c
@@ -9,13 +9,21 @@
 #include <lib/limine.h>
 #include <lib/asm.h>
 #include <lib/log.h>
+
+#if defined(__x86_64__)
 #include <arch/x64/exceptions.h>
 #include <arch/x64/idt.h>
+#else
+#error MkallOS only supports x86_64 for now
+#endif
 
 _noreturn int _start(void) {
   printk(PRINTK_INFO "Booting..\n");
+
+#if defined(__x86_64__)
   load_idt();
   init_exceptions();
+#endif
   
   asmv("cli; hlt");
   __builtin_unreachable();
