f6354df917d1a2ae545da2b4db5e9d9f9521a783
Ian Moffett <spurious.vector@gmail.com>
2022-12-14 04:52:16 -0500

    Feat: setup pmm.

--- sys/src/init.c
+++ sys/src/init.c
@@ -9,6 +9,7 @@
 #include <lib/limine.h>
 #include <lib/asm.h>
 #include <lib/log.h>
+#include <mm/pmm.h>
 
 #if defined(__x86_64__)
 #include <arch/x64/exceptions.h>
@@ -17,12 +18,19 @@
 #error MkallOS only supports x86_64 for now
 #endif
 
+
+static void init_mm(void) {
+  pmm_init();
+}
+
+
 _noreturn int _start(void) {
   printk(PRINTK_INFO "Booting..\n");
 
 #if defined(__x86_64__)
   load_idt();
   init_exceptions();
+  init_mm();
 #endif
   
   asmv("cli; hlt");
