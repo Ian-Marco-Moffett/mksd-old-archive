84768e10e9abc4e1f48cce40ec9e72ebbb2f75d4
Ian Moffett <spurious.vector@gmail.com>
2022-12-14 03:36:01 -0500

    Update init

--- sys/src/init.c
+++ sys/src/init.c
@@ -9,9 +9,14 @@
 #include <lib/limine.h>
 #include <lib/asm.h>
 #include <lib/log.h>
+#include <arch/x64/exceptions.h>
+#include <arch/x64/idt.h>
 
 _noreturn int _start(void) {
   printk(PRINTK_INFO "Booting..\n");
+  load_idt();
+  init_exceptions();
+  
   asmv("cli; hlt");
   __builtin_unreachable();
 }
