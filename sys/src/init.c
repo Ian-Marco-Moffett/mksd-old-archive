bd69675cdd5d7513c14d510edd312776daf35f6f
Ian Moffett <spurious.vector@gmail.com>
2022-12-14 03:03:26 -0500

    Feat: Add nice booting message.

--- sys/src/init.c
+++ sys/src/init.c
@@ -8,8 +8,10 @@
 
 #include <lib/limine.h>
 #include <lib/asm.h>
+#include <lib/log.h>
 
 _noreturn int _start(void) {
+  printk(PRINTK_INFO "Booting..\n");
   asmv("cli; hlt");
   __builtin_unreachable();
 }
