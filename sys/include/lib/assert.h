e0677cc53ee1b7f61578c2d05a4171604859f8b0
Ian Moffett <spurious.vector@gmail.com>
2022-12-14 19:01:49 -0500

    Feat: implement ASSERT

---
+++ sys/include/lib/assert.h
@@ -0,0 +1,11 @@
+#include <lib/asm.h>
+#include <lib/log.h>
+
+#if defined(__x86_64__)
+#define ASSERT(condition, failmsg)    \
+  if (!(condition)) {                 \
+    printk(PRINTK_KPANIC_START);      \
+    printk(PRINTK_KPANIC "ASSERTION \"" #condition "\" FAILED (%s:%d)\n", failmsg, __FILE__, __LINE__);     \
+    asmv("cli; hlt");                 \
+  }
+#endif
