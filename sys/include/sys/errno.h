1a39ec537ec18cfe30418379e882df012ffe5256
Ian Moffett <spurious.vector@gmail.com>
2022-12-16 03:47:24 -0500

    Feat: Add errno header.

---
+++ sys/include/sys/errno.h
@@ -0,0 +1,16 @@
+#ifndef _ERRNO_H_
+#define _ERRNO_H_
+
+
+#define E2BIG        1         /* Argument too long */
+#define EACCES       2         /* Permission denied */
+#define EADDRINUSE   3         /* Address in use */
+#define EAFNOSUPPORT 4         /* Address family not supported */
+#define EAGAIN       5         /* Resource unavailable, try again */
+#define EALREADY     6         /* Connection already in progress */
+#define EBADF        7         /* Bad file descriptor */
+#define EBADMSG      8         /* Bad message */
+#define EBUSY        9         /* Device or resource busy */
+#define EPERM        10        /* Operation not permitted */
+
+#endif
