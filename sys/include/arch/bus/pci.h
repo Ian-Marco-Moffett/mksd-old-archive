15ae1137f2fb23bb2fee1495e547f6b15f77616b
Ian Moffett <spurious.vector@gmail.com>
2022-12-17 18:23:35 -0500

    Misc: Update pci.h

--- sys/include/arch/bus/pci.h
+++ sys/include/arch/bus/pci.h
@@ -16,17 +16,14 @@ typedef struct
  *  Reads a word from the PCI configuration space.
  *
  */
-
-uint16_t 
-pci_config_readw(uint32_t bus, uint32_t slot, uint32_t func, uint8_t offset);
+uint16_t pci_config_readw(uint32_t bus, uint32_t slot, uint32_t func, uint8_t offset);
 
 /*
  * Writes a word to the PCI configuration space.
  *
  */
 
-void
-pci_config_writew(uint32_t bus, uint32_t slot, uint32_t func, uint8_t offset,
+void pci_config_writew(uint32_t bus, uint32_t slot, uint32_t func, uint8_t offset,
                            uint16_t data);
 
 
@@ -39,7 +36,6 @@ pci_config_writew(uint32_t bus, uint32_t slot, uint32_t func, uint8_t offset,
  *
  */
 
-void 
-pci_enable_bus_mastering(pci_device_t* dev);
+void pci_enable_bus_mastering(pci_device_t* dev);
 
 #endif
