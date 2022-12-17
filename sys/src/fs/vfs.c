e480cdbf6433d9e5c58ea17421dfcb2614e11ed7
Ian Moffett <spurious.vector@gmail.com>
2022-12-17 16:30:12 -0500

    VFS: Can now resolve filenames.

--- sys/src/fs/vfs.c
+++ sys/src/fs/vfs.c
@@ -9,9 +9,56 @@
 #include <lib/string.h>
 #include <lib/log.h>
 #include <sys/errno.h>
+#include <lib/asm.h>        // XXX: Remove this include after using
+                            // vfs_path_to_node.
 
 static vfs_node_t* root_node = NULL;
 
+
+static inline vfs_node_t*
+vfs_name_to_node(const char* name, vfs_node_t* in)
+{
+  return hashmap_read(&in->children, name);
+}
+
+// XXX: Remove the _unused later on.
+_unused static vfs_node_t*
+vfs_path_to_node(const char* path)
+{
+  if (*path != '/')
+  {
+    /* Path must must start with '/' */
+    return NULL;
+  }
+  
+  /* Skip '/' */
+  ++path;
+  
+  /* Current filename */
+  char current_fname[256];
+  size_t cur_fname_idx = 0;
+
+  /* Last node we found */
+  vfs_node_t* last_node = root_node;
+
+  for (size_t i = 0; i < strlen(path); ++i)
+  {
+    if (path[i] == '/')
+    {
+      current_fname[cur_fname_idx] = '\0';
+      last_node = vfs_name_to_node(current_fname, last_node);
+    }
+
+    if (path[i + 1] == '\0')
+    {
+      break;
+    }
+
+    current_fname[cur_fname_idx++] = path[i];
+  }
+
+  return last_node == root_node ? NULL : last_node;
+}
 
 
 int
@@ -29,7 +76,14 @@ vfs_make_node(const char* name, vfs_node_t* parent,
 
   new_node->n_children = 0;
   new_node->parent = parent;
+  new_node->is_dir = is_dir;
   memcpy(new_node->name, name, strlen(name));
+
+  if (parent != NULL)
+  {
+    hashmap_insert(&parent->children, new_node, name);
+  }
+
   return 0;
 }
 
@@ -43,5 +97,4 @@ vfs_init(void)
   }
 
   vfs_make_node("", NULL, 1, &root_node);
-  printk(PRINTK_INFO "Mounted '/'\n");
 }
